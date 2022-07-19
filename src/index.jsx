994aaee511e40a332a38709dc1bdafacb1ca5424
SmallPeenwow <bowdencamryn@gmail.com>
2022-07-19 22:56:27 +0200

    React Router Dom 6 Code Added

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
-const rootElement = document.getElementById('root');
-
-if (rootElement) {
-	const root = ReactDOM.createRoot(document.getElementById('root'));
-	root.render(
-		<React.StrictMode>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
-		</React.StrictMode>
-	);
-}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>
+);
