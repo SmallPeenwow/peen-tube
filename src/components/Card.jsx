d013957f95c944ca723fdfceb9a96e9cd46f4913
SmallPeenwow <bowdencamryn@gmail.com>
2022-07-20 20:10:46 +0200

    Styled Components Using Props Done

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,26 +3,31 @@ import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 
 const Container = styled.div`
-	width: 360px;
-	margin-bottom: 45px;
+	width: ${(props) => props.type !== 'sm' && '360px'};
+	margin-bottom: ${(props) => (props.type === 'sm' ? '10px' : '45px')};
 	cursor: pointer;
+	display: ${(props) => props.type === 'sm' && 'flex'};
+	gap: 10px;
 `;
 
 const Image = styled.img`
 	width: 100%;
-	height: 202px;
+	height: ${(props) => (props.type === 'sm' ? '120px' : '202px')};
+	flex: 1;
 `;
 
 const Details = styled.div`
 	display: flex;
-	margin-top: 16px;
+	margin-top: ${(props) => props.type !== 'sm' && '16px'};
 	gap: 12px;
+	flex: 1;
 `;
 
 const ChannelImage = styled.img`
 	width: 36px;
 	height: 36px;
 	border-radius: 50%;
+	display: ${(props) => props.type === 'sm' && 'none'};
 `;
 
 const Texts = styled.div``;
@@ -44,13 +49,13 @@ const Info = styled.div`
 	color: ${({ theme }) => theme.textSoft};
 `;
 
-const Card = () => {
+const Card = ({ type }) => {
 	return (
 		<Link to='/video/test' style={{ textDecoration: 'none' }}>
-			<Container>
-				<Image />
-				<Details>
-					<ChannelImage />
+			<Container type={type}>
+				<Image type={type} />
+				<Details type={type}>
+					<ChannelImage type={type} />
 					<Texts>
 						<Title>Test Video</Title>
 						<ChannelName>Peen Corp</ChannelName>
