10cacd296872742218cc32209af25d4dca203a7c
SmallPeenwow <bowdencamryn@gmail.com>
2022-09-28 09:43:25 +0200

    Making MongoDB Connection Done

--- server/index.js
+++ server/index.js
@@ -1,8 +1,23 @@
 //const express = require('express');
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
 const app = express();
+dotenv.config();
+
+const connect = () => {
+	mongoose
+		.connect(process.env.MONGO)
+		.then(() => {
+			console.log('Connected to DB');
+		})
+		.catch((err) => {
+			throw err;
+		});
+};
 
 app.listen(3001, () => {
+	connect();
 	console.log('connected');
 });
