ccc7a0d2720be071a67938a9ff8cf77e36fb2bd3
SmallPeenwow <bowdencamryn@gmail.com>
2022-10-01 10:05:14 +0200

    React Redux Auth User Login Done

--- client/src/components/Menu.jsx
+++ client/src/components/Menu.jsx
@@ -18,6 +18,7 @@ import FlagOutlinedIcon from '@mui/icons-material/FlagOutlined';
 import HelpOutlineOutlinedIcon from '@mui/icons-material/HelpOutlineOutlined';
 import SettingsBrightnessOutlinedIcon from '@mui/icons-material/SettingsBrightnessOutlined';
 import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 const Container = styled.div`
 	flex: 1;
@@ -93,6 +94,8 @@ const Title = styled.h2`
 `;
 
 const Menu = ({ darkMode, setDarkMode }) => {
+	const { currentUser } = useSelector((state) => state.user);
+
 	return (
 		<Container>
 			<Wrapper>
@@ -128,16 +131,20 @@ const Menu = ({ darkMode, setDarkMode }) => {
 					History
 				</Item>
 				<Hr />
-				<Login>
-					Sing in to like videos, comment, and subscribe.
-					<Link to='signin' style={{ textDecoration: 'none' }}>
-						<Button>
-							<AccountCircleOutlinedIcon />
-							SIGN IN
-						</Button>
-					</Link>
-				</Login>
-				<Hr />
+				{!currentUser && (
+					<>
+						<Login>
+							Sing in to like videos, comment, and subscribe.
+							<Link to='signin' style={{ textDecoration: 'none' }}>
+								<Button>
+									<AccountCircleOutlinedIcon />
+									SIGN IN
+								</Button>
+							</Link>
+						</Login>
+						<Hr />
+					</>
+				)}
 				<Title>BEST OF PEENCORP</Title>
 				<Item>
 					<LibraryMusicOutlinedIcon />
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
 import styled from 'styled-components';
 import axios from 'axios';
+import { loginFailure, loginStart, loginSuccess } from '../redux/userSlice';
 
 const Container = styled.div`
 	display: flex;
@@ -67,13 +69,18 @@ const SignIn = () => {
 	const [name, setName] = useState('');
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
+	const dispatch = useDispatch();
 
 	const handleLogin = async (e) => {
 		e.preventDefault();
+		dispatch(loginStart());
 
 		try {
 			const res = await axios.post('/auth/signin', { name, password });
-		} catch (err) {}
+			dispatch(loginSuccess(res.data));
+		} catch (err) {
+			dispatch(loginFailure());
+		}
 	};
 
 	return (
