c73490abb8ba356f231ea96d61fb6c2047024616
SmallPeenwow <bowdencamryn@gmail.com>
2022-10-02 14:30:31 +0200

    React Upload Files using Firebase Storage Done

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
 
 const Container = styled.div`
 	width: 100%;
@@ -68,18 +69,69 @@ const Label = styled.label`
 `;
 
 const Upload = ({ setOpen }) => {
+	const [img, setImg] = useState(null);
+	const [video, setVideo] = useState(null);
+	const [imgPerc, setImgPerc] = useState(0);
+	const [videoPerc, setVideoPerc] = useState(0);
+	const [inputs, setInputs] = useState({});
+	const [tags, setTags] = useState([]);
+
+	const handleChange = (e) => {
+		setInputs((prev) => {
+			return { ...prev, [e.target.name]: e.target.value };
+		});
+	};
+
+	const handleTags = (e) => {
+		setTags(e.target.value.split(','));
+	};
+
+	const uploadFile = (file, urlType) => {
+		const storage = getStorage();
+		const fileName = new Date().getTime() + file.name;
+		const storageRef = ref(storage, fileName);
+		const uploadTask = uploadBytesResumable(storageRef, file);
+
+		uploadTask.on(
+			'state_changed',
+			(snapshot) => {
+				const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+				urlType === 'imgUrl' ? setImgPerc(Math.round(progress)) : setVideoPerc(Math.round(progress));
+				switch (snapshot.state) {
+					case 'paused':
+						console.log('Upload is paused');
+						break;
+					case 'running':
+						console.log('Upload is running');
+						break;
+					default:
+						break;
+				}
+			},
+			(error) => {}
+		);
+	};
+
+	useEffect(() => {
+		uploadFile(video);
+	}, [video]);
+
+	useEffect(() => {
+		uploadFile(img);
+	}, [img]);
+
 	return (
 		<Container>
 			<Wrapper>
 				<Close onClick={() => setOpen(false)}>X</Close>
 				<Title>Upload a New Video</Title>
 				<Label>Video:</Label>
-				<Input type='file' accept='video/*' />
-				<Input type='text' placeholder='Title' />
-				<Desc placeholder='Description' rows={8} />
-				<Input type='text' placeholder='Separate the tags with commas.' />
+				<Input type='file' accept='video/*' onChange={(e) => setVideo(e.target.files[0])} />
+				<Input type='text' placeholder='Title' name='title' onChange={handleChange} />
+				<Desc placeholder='Description' name='desc' rows={8} onChange={handleChange} />
+				<Input type='text' placeholder='Separate the tags with commas.' onChange={handleTags} />
 				<Label>Image:</Label>
-				<Input type='file' placeholder='image/*' />
+				<Input type='file' placeholder='image/*' onChange={(e) => setImg(e.target.files[0])} />
 				<Button>Upload</Button>
 			</Wrapper>
 		</Container>
